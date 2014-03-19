031802c5e0942b02e7df12edb85a8af695ea5509
MBulli <markusbullmann@gmail.com>
2014-03-19 22:08:31 +0100

    Removed CocoaPods

--- Crystal/Pods/Pods-Crystal-EstimoteSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Crystal_EstimoteSDK : NSObject
-@end
-@implementation PodsDummy_Pods_Crystal_EstimoteSDK
-@end
--- Crystal/Pods/Pods-Crystal-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Crystal : NSObject
-@end
-@implementation PodsDummy_Pods_Crystal
-@end
