5aaeefba17ddbe1154df5c1170c4c88e605deb5c
Steffen Drews <drewssteffen@gmail.com>
2014-03-20 01:17:30 +0100

    navi controller startscreen initialscreen

---
+++ Crystal/Crystal/CRYInitialScreenController.m
@@ -0,0 +1,49 @@
+//
+//  CRYInitialScreenController.m
+//  Crystal
+//
+//  Created by Steffen on 20.03.14.
+//  Copyright (c) 2014 Crystal Corp. All rights reserved.
+//
+
+#import "CRYInitialScreenController.h"
+
+@interface CRYInitialScreenController ()
+
+@end
+
+@implementation CRYInitialScreenController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
