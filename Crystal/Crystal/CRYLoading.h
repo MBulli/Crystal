941d0d1616817fd1e26782e7aee161ae817f11f7
Markus <markusbullmann@gmail.com>
2014-03-20 05:44:31 +0100

    added CRYLoading

---
+++ Crystal/Crystal/CRYLoading.h
@@ -0,0 +1,13 @@
+//
+//  CRYLoading.h
+//  Crystal
+//
+//  Created by Markus on 3/20/14.
+//  Copyright (c) 2014 Crystal Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRYLoading : UIView
+
+@end
